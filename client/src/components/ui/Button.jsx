d1e2ebb3b4280ad25d01ae5fc30ab88590ea3530
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-01 17:48:22 +0545

    create button component

---
+++ client/src/components/ui/Button.jsx
@@ -0,0 +1,22 @@
+import clsx from "clsx"
+
+const Button = ({children,className,variant="primary",size="md"}) => {
+
+    const variants = {
+        primary: "bg-primary text-white hover:bg-primary/60",
+        ghost: "hover:underline text-primary",
+    }
+
+    const sizes = {
+        sm: 'px-2 py-1',
+        md: 'px-4 py-2',
+        lg: 'px-6 py-3'
+    }
+   
+  return (
+    <button className={clsx('cursor-pointer  rounded-full',className,variants[variant],sizes[size])}>
+        {children}
+    </button>
+  )
+}
+export default Button
