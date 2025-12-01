c69a8bd53eccbbb627ae77760d79eeef6a26b9f1
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-01 17:48:44 +0545

    add clsx for dynamic class

--- client/src/components/MaxWidthContainer.jsx
+++ client/src/components/MaxWidthContainer.jsx
@@ -1,6 +1,8 @@
+import clsx from "clsx"
+
 const MaxWidthContainer = ({children,className}) => {
   return (
-    <div className={`max-w-7xl mx-auto px-4 md:px-6 ${className}`}>
+    <div className={clsx('max-w-7xl mx-auto px-4 md:px-6',className)}>
         {children}
     </div>
   )
