7b4e9f7213cab8e2a7ac2f1c11af9d78ff6501c4
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-03 17:27:24 +0545

    add hero section and new arrival component to homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
-import MaxWidthContainer from "../components/MaxWidthContainer";
+import HeroSection from "../components/HeroSection"
+import MaxWidthContainer from "../components/MaxWidthContainer"
+import NewArrivals from "../components/NewArrivals"
 
 const HomePage = () => {
-  return <MaxWidthContainer>HomePage</MaxWidthContainer>;
-};
-export default HomePage;
+  return (
+    <div>
+      <HeroSection />
+
+      {/* new arrivals */}
+      <MaxWidthContainer>
+        <h1 className="text-slate-800 text-2xl font-semibold mb-6">
+          New Arrivals
+        </h1>
+        <NewArrivals />
+      </MaxWidthContainer>
+
+    </div>
+  )
+}
+export default HomePage
