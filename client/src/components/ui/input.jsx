60fb18fd277fa585ee282428efdac89295fa1441
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-11 08:30:50 +0545

    add ui componenet from shadcn ui

--- client/src/components/ui/input.jsx
+++ client/src/components/ui/input.jsx
@@ -1,19 +1,35 @@
-import cn from "../../libs/cn"
+import cn from "../../libs/cn";
+import * as LabelPrimitive from "@radix-ui/react-label"
 
-function Input({ className, type, ...props }) {
+function Input({ className, type, label="", id="",  ...props }) {
   return (
-    <input
-      type={type}
-      data-slot="input"
+    <>
+    {
+      label &&   <LabelPrimitive.Root
+      data-slot="label"
+      id={id}
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50"
       )}
-      {...props}
-    />
-  )
+    >
+      {label}
+    </LabelPrimitive.Root>
+
+    }
+      <input
+        type={type}
+        id={id}
+        data-slot="input"
+        className={cn(
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+          className
+        )}
+        {...props}
+      />
+    </>
+  );
 }
 
-export { Input }
+export { Input };
