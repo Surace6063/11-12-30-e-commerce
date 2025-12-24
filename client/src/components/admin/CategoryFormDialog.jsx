8f267ef42378b6190452aca5582b91414d8d665c
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-24 17:17:48 +0545

    create category form dialog

---
+++ client/src/components/admin/CategoryFormDialog.jsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import {Plus} from "lucide-react"
+
+const CategoryFormDialog = () => {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button className="rounded-xl gap-2 bg-slate-800 text-white hover:bg-slate-700 transition">
+          <Plus className="h-4 w-4" />
+          Add Category
+        </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader className="mb-6">
+            <DialogTitle>Add Category</DialogTitle>
+          </DialogHeader>
+            <Input type="text" placeholder="Enter category name" label="Name" id="name" />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  )
+}
+export default CategoryFormDialog
