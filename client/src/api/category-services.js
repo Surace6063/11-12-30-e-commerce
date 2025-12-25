a357888d2fa44a4f6da57a579197cf762cb126cf
Suresh Thapa <sureshthapa6063@gmail.com>
2025-12-25 17:23:49 +0545

    add category management hooks

--- client/src/api/category-services.js
+++ client/src/api/category-services.js
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { apiRequest } from "../libs/apiRequest";
 
 // get categories
@@ -10,3 +10,37 @@ export const useCategories = () =>
       return response.data;
     },
   });
+
+// add category  
+export const useAddCategory = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (data) => await apiRequest.post('/categories/',data),
+    onSuccess: () => queryClient.invalidateQueries(['categories']),
+    onError: (error) => console.log(error)
+  })
+}
+
+// delete category
+export const useDeleteCategory = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (id) => await apiRequest.delete(`/categories/${id}/`),
+    onSuccess: () => queryClient.invalidateQueries(['categories']),
+    onError: (error) => console.log(error)
+  })
+}
+
+
+// update category
+export const useUpdateCategory = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({id, data}) => await apiRequest.patch(`/categories/${id}/`,data),
+    onSuccess: () => queryClient.invalidateQueries(['categories']),
+    onError: (error) => console.log(error)
+  })
+}
